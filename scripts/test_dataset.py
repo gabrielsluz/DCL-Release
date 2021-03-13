61faa2031ce9e83214823ccef1bf8ccb7f13ecf8
gabrielsluz <gabrielsluz1999@gmail.com>
2021-03-13 14:58:12 -0300

    Initialize dataset

--- scripts/test_dataset.py
+++ scripts/test_dataset.py
@@ -1,9 +1,11 @@
 #! /usr/bin/env python3
 ## -*- coding: utf-8 -*-
 
+from nscl.datasets import get_available_datasets, initialize_dataset, get_dataset_builder
 from clevrer.dataset_clevrer import build_clevrer_dataset  
 from opts import load_param_parser 
 
+initialize_dataset(args.dataset, args.version)
 args = load_param_parser()
 train_dataset = build_clevrer_dataset(args, 'train')
 train_dataloader = train_dataset.make_dataloader(args.batch_size, shuffle=False, drop_last=True, nr_workers=args.data_workers)
