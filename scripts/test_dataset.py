cd439267274ad2d4abec69cba130e30c2bdb1df7
gabrielsluz <gabrielsluz1999@gmail.com>
2021-03-14 01:06:23 -0300

    Comment train

--- scripts/test_dataset.py
+++ scripts/test_dataset.py
@@ -7,20 +7,20 @@ from opts import load_param_parser
 
 args = load_param_parser()
 initialize_dataset(args.dataset, args.version)
-train_dataset = build_clevrer_dataset(args, 'train')
-train_dataloader = train_dataset.make_dataloader(args.batch_size, shuffle=False, drop_last=True, nr_workers=args.data_workers)
+# train_dataset = build_clevrer_dataset(args, 'train')
+# train_dataloader = train_dataset.make_dataloader(args.batch_size, shuffle=False, drop_last=True, nr_workers=args.data_workers)
 
-print("Dataset Train len = {}".format(len(train_dataset)))
-train_iter = iter(train_dataloader)
-cur_iter = 0
-while True:
-    try:
-        feed_dict = next(train_iter)
-    except StopIteration: 
-        break
-    except:
-        print("Failed to load item {}".format(cur_iter))
-    cur_iter += 1
+# print("Dataset Train len = {}".format(len(train_dataset)))
+# train_iter = iter(train_dataloader)
+# cur_iter = 0
+# while True:
+#     try:
+#         feed_dict = next(train_iter)
+#     except StopIteration: 
+#         break
+#     except:
+#         print("Failed to load item {}".format(cur_iter))
+#     cur_iter += 1
         
 """
 Failed to load item 991
