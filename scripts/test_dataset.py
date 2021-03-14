2cfa91a662fd3e969235a1fecebb833b2e7b9d57
gabrielsluz <gabrielsluz1999@gmail.com>
2021-03-14 01:04:10 -0300

    Merge

--- scripts/test_dataset.py
+++ scripts/test_dataset.py
@@ -7,10 +7,10 @@ from opts import load_param_parser
 
 args = load_param_parser()
 initialize_dataset(args.dataset, args.version)
-train_dataset = build_clevrer_dataset(args, 'validation')
+train_dataset = build_clevrer_dataset(args, 'train')
 train_dataloader = train_dataset.make_dataloader(args.batch_size, shuffle=False, drop_last=True, nr_workers=args.data_workers)
 
-print("Dataset len = {}".format(len(train_dataset)))
+print("Dataset Train len = {}".format(len(train_dataset)))
 train_iter = iter(train_dataloader)
 cur_iter = 0
 while True:
@@ -30,3 +30,37 @@ Failed to load item 2527
 Failed to load item 2768
 Failed to load item 2955
 """
+
+initialize_dataset(args.dataset, args.version)
+train_dataset = build_clevrer_dataset(args, 'validation')
+train_dataloader = train_dataset.make_dataloader(args.batch_size, shuffle=False, drop_last=True, nr_workers=args.data_workers)
+
+print("Dataset Val len = {}".format(len(train_dataset)))
+train_iter = iter(train_dataloader)
+cur_iter = 0
+while True:
+    try:
+        feed_dict = next(train_iter)
+    except StopIteration: 
+        break
+    except:
+        print("Failed to load item {}".format(cur_iter))
+    cur_iter += 1
+        
+
+initialize_dataset(args.dataset, args.version)
+train_dataset = build_clevrer_dataset(args, 'test')
+train_dataloader = train_dataset.make_dataloader(args.batch_size, shuffle=False, drop_last=True, nr_workers=args.data_workers)
+
+print("Dataset Test len = {}".format(len(train_dataset)))
+train_iter = iter(train_dataloader)
+cur_iter = 0
+while True:
+    try:
+        feed_dict = next(train_iter)
+    except StopIteration: 
+        break
+    except:
+        print("Failed to load item {}".format(cur_iter))
+    cur_iter += 1
+        
