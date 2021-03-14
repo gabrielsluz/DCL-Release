d04c235b7299456757fc5eb2a7ab7af070f02165
gabrielsluz <gabrielsluz1999@gmail.com>
2021-03-14 00:53:03 -0300

    validation and test

--- scripts/test_dataset.py
+++ scripts/test_dataset.py
@@ -7,10 +7,10 @@ from opts import load_param_parser
 
 args = load_param_parser()
 initialize_dataset(args.dataset, args.version)
-train_dataset = build_clevrer_dataset(args, 'train')
+train_dataset = build_clevrer_dataset(args, 'validation')
 train_dataloader = train_dataset.make_dataloader(args.batch_size, shuffle=False, drop_last=True, nr_workers=args.data_workers)
 
-print("Dataset Train len = {}".format(len(train_dataset)))
+print("Dataset len = {}".format(len(train_dataset)))
 train_iter = iter(train_dataloader)
 cur_iter = 0
 while True:
@@ -22,3 +22,11 @@ while True:
         print("Failed to load item {}".format(cur_iter))
     cur_iter += 1
         
+"""
+Failed to load item 991
+Failed to load item 2146
+Failed to load item 2280
+Failed to load item 2527
+Failed to load item 2768
+Failed to load item 2955
+"""
